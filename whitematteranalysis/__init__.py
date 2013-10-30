acf2696cc74695873f9d2cb5959c4cecf6620030
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2013-10-30 12:17:59 -0400

    ENH: Remove unused code

--- whitematteranalysis/__init__.py
+++ whitematteranalysis/__init__.py
@@ -1,5 +1,4 @@
-#__all__ = ["io", "fibers", "laterality", "register", "render", "test"]
-for m in ["io", "fibers", "filter", "laterality", "midsagalign", "register", "render", "cluster", "relative_distance", "congeal", "registration_functions"]:
+for m in ["io", "fibers", "filter", "laterality", "render", "cluster", "relative_distance", "congeal", "registration_functions"]:
   #print "importing ", m
   exec 'import %s'%m
 
