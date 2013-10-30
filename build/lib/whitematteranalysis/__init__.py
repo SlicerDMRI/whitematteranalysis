f15474c0bb38b0d6fb0809c9f1ebaf14c89b5019
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2013-10-30 11:53:37 -0400

    FIX: removed autogenerated build directory from repo

--- build/lib/whitematteranalysis/__init__.py
+++
@@ -1,5 +0,0 @@
-#__all__ = ["io", "fibers", "laterality", "register", "render", "test"]
-for m in ["io", "fibers", "filter", "laterality", "midsagalign", "register", "render", "cluster", "relative_distance", "congeal", "registration_functions"]:
-  #print "importing ", m
-  exec 'import %s'%m
-
