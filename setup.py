989c186fb4e8dd428f9311e23742fd754467d5ee
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2013-10-30 12:42:06 -0400

    FIX: setup does not look for empty removed test directory now

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     version='0.1.0',
     author='Lauren O\'Donnell',
     author_email='odonnell@bwh.harvard.edu',
-    packages=['whitematteranalysis', 'whitematteranalysis.test'],
+    packages=['whitematteranalysis'],
     license='LICENSE.txt',
     description='Processing of whole-brain streamline tractography.',
     long_description=open('README.txt').read(),
