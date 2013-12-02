c036af58f23b9aa064cbe76a81eb7a4a6cbb603c
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2013-12-02 17:39:18 -0500

    ENH downsample fibers for render

--- bin-test/view_li.py
+++ bin-test/view_li.py
@@ -1,5 +1,5 @@
 import whitematteranalysis as wma
 pd = wma.io.read_polydata('tractography_with_LI.vtk')
-ren = wma.render.render(pd, scalar_range=[-.5,.5], scalar_bar=True)
+ren = wma.render.render(pd, scalar_range=[-.5,.5], scalar_bar=True, number_of_fibers=1000)
 ren.save_views()
 
