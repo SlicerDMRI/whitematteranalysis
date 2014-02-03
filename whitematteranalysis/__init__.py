fd98f49d437b985d8e7cf8ee9e111a68fc3332ee
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2014-02-03 11:04:15 -0500

    NEW: add simple MRML writing for clusters, for visualization with colors in slicer

--- whitematteranalysis/__init__.py
+++ whitematteranalysis/__init__.py
@@ -1,4 +1,4 @@
-for m in ["io", "fibers", "filter", "laterality", "render", "cluster", "relative_distance", "congeal", "registration_functions"]:
+for m in ["io", "fibers", "filter", "laterality", "render", "cluster", "relative_distance", "congeal", "registration_functions", "mrml"]:
   #print "importing ", m
   exec 'import %s'%m
 
---
+++ whitematteranalysis/mrml.py
@@ -0,0 +1,136 @@
+""" mrml.py
+
+output in MRML scene file format (XML)
+
+"""
+import os
+
+def write(pd_filenames, colors, filename):
+    writer = WriteMRML()
+    writer.write(pd_filenames, colors, filename)
+    del writer
+    print "LAUREN figure out subsampling ratio that is reasonable for the data"
+    
+class WriteMRML:
+    
+    def __init__(self):
+        self.header = '<MRML  version="Slicer4" userTags="">\n'
+        self.footer = '</MRML>\n'
+        self.indent = ' '
+        self.node_id = 0
+        self.props_id = 0
+        
+    def write(self, pd_filenames, colors, filename):
+        #print "converting colors to strings"
+        color_list = list()
+        for cidx in range(len(colors)):
+            col = str(colors[cidx,0]/256.0) + " " + str(colors[cidx,1]/256.0) + " " + str(colors[cidx,2]/256.0)
+            color_list.append(col)
+        #print color_list
+        print "writing", len(pd_filenames), "cluster filenames to MRML scene:", filename
+        f = open(filename, "w")
+        f.write(self.header)
+        for pidx in range(len(pd_filenames)):
+            name = os.path.splitext(os.path.split(pd_filenames[pidx])[1])[0]
+            self.write_node(pd_filenames[pidx], color_list[pidx], name, f)
+        f.write(self.footer)
+        f.close()
+        
+    def write_node(self, pd_fname, color, name, f):
+
+        self.node_id += 1
+        idx = self.node_id
+        props = list()
+        
+        f.write(self.indent)
+        f.write("<FiberBundleStorage\n")
+        f.write(self.indent)
+        f.write(self.indent)
+        f.write("id=\"vtkMRMLFiberBundleStorageNode" + str(idx) + "\"  ")
+        f.write("name=\"FiberBundleStorage\"  hideFromEditors=\"true\"  selectable=\"true\"  selected=\"false\"  ")
+        f.write("fileName=\"" + pd_fname+ "\"  ")
+        f.write("useCompression=\"1\"  readState=\"0\"  writeState=\"0\" ></FiberBundleStorage>")
+        f.write("\n")
+                
+        self.props_id += 1
+        idxp = self.props_id
+        props.append(idxp)
+
+        f.write(self.indent)
+        f.write("<FiberBundleLineDisplayNode\n")
+        f.write(self.indent)
+        f.write(self.indent)
+        f.write("id=\"vtkMRMLFiberBundleLineDisplayNode" + str(idx) + "\"  ")
+        f.write("name=\"FiberBundleLineDisplayNode\"  hideFromEditors=\"true\"  selectable=\"true\"  selected=\"false\"  ")
+        f.write("color=\"" + color + "\"  ")
+        f.write("edgeColor=\"0 0 0\"  selectedColor=\"1 0 0\"  selectedAmbient=\"0.4\"  ambient=\"0\"  diffuse=\"1\"  selectedSpecular=\"0.5\"  specular=\"0\"  power=\"1\"  opacity=\"1\"  pointSize=\"1\"  lineWidth=\"1\"  representation=\"2\"  lighting=\"true\"  interpolation=\"1\"  shading=\"true\"  visibility=\"true\"  edgeVisibility=\"false\"  clipping=\"false\"  sliceIntersectionVisibility=\"false\"  sliceIntersectionThickness=\"1\"  frontfaceCulling=\"false\"  backfaceCulling=\"false\"  scalarVisibility=\"true\"  vectorVisibility=\"false\"  tensorVisibility=\"false\"  interpolateTexture=\"false\"  autoScalarRange=\"true\"  scalarRange=\"0 1\"  colorNodeID=\"vtkMRMLColorTableNodeRainbow\"   colorMode =\"1\"  ")
+        f.write("DiffusionTensorDisplayPropertiesNodeRef=\"vtkMRMLDiffusionTensorDisplayPropertiesNode" + str(idxp) + "\"  ")
+        f.write("></FiberBundleLineDisplayNode>")
+        f.write("\n")
+
+        self.props_id += 1
+        idxp = self.props_id
+        props.append(idxp)
+        
+        f.write(self.indent)
+        f.write("<FiberBundleTubeDisplayNode\n")
+        f.write(self.indent)
+        f.write(self.indent)
+        f.write("id=\"vtkMRMLFiberBundleTubeDisplayNode" + str(idx) + "\"  ")
+        f.write("name=\"FiberBundleTubeDisplayNode\"  hideFromEditors=\"true\"  selectable=\"true\"  selected=\"false\"  ")
+        f.write("color=\"" + color + "\"  ")
+        f.write("edgeColor=\"0 0 0\"  selectedColor=\"1 0 0\"  selectedAmbient=\"0.4\"  ambient=\"0.25\"  diffuse=\"0.8\"  selectedSpecular=\"0.5\"  specular=\"0.25\"  power=\"20\"  opacity=\"1\"  pointSize=\"1\"  lineWidth=\"1\"  representation=\"2\"  lighting=\"true\"  interpolation=\"1\"  shading=\"true\"  visibility=\"false\"  edgeVisibility=\"false\"  clipping=\"false\"  sliceIntersectionVisibility=\"false\"  sliceIntersectionThickness=\"1\"  frontfaceCulling=\"false\"  backfaceCulling=\"false\"  scalarVisibility=\"true\"  vectorVisibility=\"false\"  tensorVisibility=\"false\"  interpolateTexture=\"false\"  autoScalarRange=\"true\"  scalarRange=\"0 1\"  colorNodeID=\"vtkMRMLColorTableNodeRainbow\"   colorMode =\"1\"  ")
+        f.write("DiffusionTensorDisplayPropertiesNodeRef=\"vtkMRMLDiffusionTensorDisplayPropertiesNode" + str(idxp) + "\"  ")
+        f.write("tubeRadius =\"0.5\"  tubeNumberOfSides =\"6\" ></FiberBundleTubeDisplayNode>")
+        f.write("\n")
+        
+        self.props_id += 1
+        idxp = self.props_id
+        props.append(idxp)
+
+        f.write(self.indent)
+        f.write("<FiberBundleGlyphDisplayNode\n")
+        f.write(self.indent)
+        f.write(self.indent)
+        f.write("id=\"vtkMRMLFiberBundleGlyphDisplayNode" + str(idx) + "\"  ")
+        f.write("name=\"FiberBundleGlyphDisplayNode\"  hideFromEditors=\"true\"  selectable=\"true\"  selected=\"false\"  ")
+        f.write("color=\"" + color + "\"  ")
+        f.write("edgeColor=\"0 0 0\"  selectedColor=\"1 0 0\"  selectedAmbient=\"0.4\"  ambient=\"0\"  diffuse=\"1\"  selectedSpecular=\"0.5\"  specular=\"0\"  power=\"1\"  opacity=\"1\"  pointSize=\"1\"  lineWidth=\"1\"  representation=\"2\"  lighting=\"true\"  interpolation=\"1\"  shading=\"true\"  visibility=\"false\"  edgeVisibility=\"false\"  clipping=\"false\"  sliceIntersectionVisibility=\"false\"  sliceIntersectionThickness=\"1\"  frontfaceCulling=\"false\"  backfaceCulling=\"false\"  scalarVisibility=\"false\"  vectorVisibility=\"false\"  tensorVisibility=\"false\"  interpolateTexture=\"false\"  autoScalarRange=\"true\"  scalarRange=\"0 1\"  colorNodeID=\"vtkMRMLColorTableNodeRainbow\"   colorMode =\"1\"  ")
+        f.write("DiffusionTensorDisplayPropertiesNodeRef=\"vtkMRMLDiffusionTensorDisplayPropertiesNode" + str(idxp) + "\"  ")
+        f.write("twoDimensionalVisibility=\"false\" ></FiberBundleGlyphDisplayNode>")
+        f.write("\n")
+
+
+        f.write(self.indent)
+        f.write("<FiberBundle\n")
+        f.write(self.indent)
+        f.write(self.indent)
+        f.write("id=\"vtkMRMLFiberBundleNode" + str(idx) + "\"  ")
+        f.write("name=\"" + name + "\"  ")
+        f.write("hideFromEditors=\"false\"  selectable=\"true\"  selected=\"false\"  ")
+        f.write("displayNodeRef=\"vtkMRMLFiberBundleLineDisplayNode" + str(idx) + "  ")
+        f.write("vtkMRMLFiberBundleTubeDisplayNode" + str(idx) + "  ")
+        f.write("vtkMRMLFiberBundleGlyphDisplayNode" + str(idx) + "\"  ")
+        f.write("storageNodeRef=\"vtkMRMLFiberBundleStorageNode" + str(idx) + "\"  ")
+        f.write("references=\"display:")
+        f.write("vtkMRMLFiberBundleLineDisplayNode" + str(idx) + "  ")
+        f.write("vtkMRMLFiberBundleTubeDisplayNode" + str(idx) + "  ")
+        f.write("vtkMRMLFiberBundleGlyphDisplayNode" + str(idx) + ";")
+        f.write("storage:")
+        f.write("vtkMRMLFiberBundleStorageNode" + str(idx) + ";\"  ")
+        f.write("userTags=\"\"  SelectWithAnnotationNode=\"0\"  SelectionWithAnnotationNodeMode=\"0\"  SubsamplingRatio=\"1\" ></FiberBundle>")
+        f.write("\n")
+
+        for prop in props:
+            self.write_prop_node(prop, f)
+        
+
+    def write_prop_node(self, idx, f):
+
+        f.write(self.indent)
+        f.write("<DiffusionTensorDisplayProperties\n")
+        f.write(self.indent)
+        f.write(self.indent)
+        f.write("id=\"vtkMRMLDiffusionTensorDisplayPropertiesNode" + str(idx) + "\"  ")
+        f.write("name=\"DiffusionTensorDisplayPropertiesNode\"  description=\"A user defined colour table, use the editor to specify it\"  hideFromEditors=\"true\"  selectable=\"true\"  selected=\"false\"  userTags=\"\" type=\"13\" numcolors=\"0\"  glyphGeometry=\"2\"  colorGlyphBy=\"3\"  glyphScaleFactor=\"50\"  glyphEigenvector=\"1\"  glyphExtractEigenvalues=\"1\"  lineGlyphResolution=\"20\"  tubeGlyphRadius=\"0.1\"  tubeGlyphNumberOfSides=\"4\"  ellipsoidGlyphThetaResolution=\"9\"  ellipsoidGlyphPhiResolution=\"9\"  superquadricGlyphGamma=\"1\"  superquadricGlyphThetaResolution=\"6\"  superquadricGlyphPhiResolution=\"6\" ></DiffusionTensorDisplayProperties>")
+        f.write("\n")
