0bf868b78be4dfff10c33ee0c4ecfeddec8763fc
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2014-02-03 11:38:36 -0500

    TEST for mrml writer

---
+++ tests/test_write_mrml.py
@@ -0,0 +1,14 @@
+import numpy
+import whitematteranalysis as wma
+
+pd_filenames = list()
+pd_filenames.append('test_data_small/brain_0001.vtk')
+pd_filenames.append('test_data_small/brain_0002.vtk')
+colors = list()
+colors.append([255, 0, 100])
+colors.append([0, 200, 150])
+colors = numpy.array(colors)
+wma.mrml.write(pd_filenames, colors , 'try.mrml')
+
+print 'Opening try.mrml in slicer should display a red and a green fiber bundle.'
+print 'try.mrml must be in the test directory for this to work. It references data in test_data_small.'
