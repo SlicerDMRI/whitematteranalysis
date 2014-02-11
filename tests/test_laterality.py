172deefb18f38a68e969aa4b409940d6952116b9
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2014-02-11 14:13:40 -0500

    Add testing for lower level similarity computation

--- tests/test_laterality.py
+++ tests/test_laterality.py
@@ -34,12 +34,12 @@ print "QUANTITATIVE RESULTS (CODE CHANGE) TESTING"
 print "------------------------------------------"
 expected_results_file = 'test_laterality.pkl'
 laterality_results = laterality.compute(pd)
-# to store any new output distances
+# to store any new output lateralities
 #####pickle.dump(laterality_results.laterality_index, open(expected_results_file, 'wb'))
 # Open saved output file, check for differences
 expected_laterality = pickle.load(open(expected_results_file, 'rb'))
 
-print "Testing for differences in saved distance output and current:"
+print "Testing for differences in saved laterality output and current:"
 error = numpy.array(expected_laterality) - laterality_results.laterality_index
 print "difference range (should be 0.0):", numpy.min(error), numpy.max(error)
 
@@ -79,10 +79,11 @@ for rand_idx in range(3):
     mask_rand = numpy.random.permutation(pd_symm.GetNumberOfLines()) > number_of_rejected_fibers - 1
     pd_rand = wma.filter.mask(pd_symm, mask_rand)
 
-    hem = mask_hem[mask_rand]
     laterality_results = laterality.compute(pd_rand)
+    mask_hem = laterality_results.hemisphere != 0
+
     print "These should be from -1 to +1 for a brain with", number_of_rejected_fibers, "randomly asymmetric fibers"
-    print "LI range (mean, median):", numpy.min(laterality_results.laterality_index), numpy.max(laterality_results.laterality_index), "(", numpy.mean(laterality_results.laterality_index[hem]), ",", numpy.median(laterality_results.laterality_index[hem]), ")"
+    print "LI range (mean, median):", numpy.min(laterality_results.laterality_index), numpy.max(laterality_results.laterality_index), "(", numpy.mean(laterality_results.laterality_index[mask_hem]), ",", numpy.median(laterality_results.laterality_index[mask_hem]), ")"
 
 
 print ""
@@ -103,4 +104,47 @@ for equal_fibers in test_equal_fibers:
 
 
 
+print ""
+print "TESTING SIMILARITY COMPUTATION"
+print "----------------------------"
+# Now test the similarity computation
+# fiber array of all fibers
+# fiber array of all fibers minus self
+# self fiber
+threshold = 0.0
+sigma = 30.0
+#mask_rand = numpy.random.permutation(pd.GetNumberOfLines()) > 0
+#fiber_idx = numpy.nonzero(~mask_rand)[0]
+pd_minus_fiber = wma.filter.mask(pd, mask_rand)
+fiber_array_minus_fiber = wma.fibers.FiberArray()
+# must request hemisphere computation from object
+fiber_array_minus_fiber.hemispheres = True
+fiber_array_minus_fiber.convert_from_polydata(pd_minus_fiber, points_per_fiber=20)
 
+
+
+fiber_array_all = wma.fibers.FiberArray()
+# must request hemisphere computation from object
+fiber_array_all.hemispheres = True
+fiber_array_all.convert_from_polydata(pd, points_per_fiber=20)
+
+fiber_idx = fiber_array_all.index_right_hem[0]
+fiber = fiber_array_all.get_fiber(fiber_idx)
+
+fiber_array_right = fiber_array_all.get_fibers(fiber_array_all.index_right_hem)
+fiber_array_left = fiber_array_all.get_fibers(fiber_array_all.index_left_hem)
+
+
+dist = wma.similarity.fiber_distance(fiber, fiber_array_right, threshold=threshold, distance_method='StrictSimilarity', sigmasq=sigma*sigma)
+
+print "Range of fiber similarities within same hem (should be small to 1.0):", numpy.min(dist), numpy.max(dist)
+print "Total fiber similarities within same hem (should be > 1.0):", numpy.sum(dist)
+
+print "Total similarity. Should be same as the above value:", \
+  wma.similarity.total_similarity_for_laterality(fiber, 
+                                                 fiber_array_right,
+                                                 fiber_array_all.is_left_hem[fiber_idx],
+                                                 threshold,
+                                                 sigma*sigma)
+
+                
