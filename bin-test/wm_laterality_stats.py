5e05534ec76a3adb72ebe02926061c0bac8944ec
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2014-02-11 14:15:09 -0500

    Cleanup, argument parsing, better print info

--- bin-test/wm_laterality_stats.py
+++ bin-test/wm_laterality_stats.py
@@ -1,18 +1,74 @@
 import glob
+import os
+import argparse
 
 import scipy.stats
 import numpy
 import matplotlib.pyplot as plt
 import scipy.signal
 
-import whitematteranalysis as wma
+try:
+    import whitematteranalysis as wma
+except:
+    print "<wm_laterality_stats.py> Error importing white matter analysis package\n"
+    raise
 
-inputDirectory = '../.'
 
-inputMask1 = "{0}/*".format(inputDirectory);
-inputDirs = sorted(glob.glob(inputMask1))
+fname_to_test_if_output = 'tractography_with_LI.vtk'
 
-print inputDirs
+
+#-----------------
+# Parse arguments
+#-----------------
+parser = argparse.ArgumentParser(
+    description="Computes summary statistics about laterality results.",
+    epilog="Written by Lauren O\'Donnell, odonnell@bwh.harvard.edu")
+
+parser.add_argument("-v", "--version",
+    action="version", default=argparse.SUPPRESS,
+    version='1.0',
+    help="Show program's version number and exit")
+parser.add_argument(
+    'inputDirectory',
+    help='Contains laterality results, one directory per subject.')
+parser.add_argument(
+    'outputDirectory',
+    help='The output directory should be a new empty directory. It will be created if needed.')
+parser.add_argument(
+    '-verbose', action='store_true', dest="flag_verbose",
+    help='Verbose. Print extra diagnostic information to the screen.')
+
+
+args = parser.parse_args()
+
+if not os.path.isdir(args.inputDirectory):
+    print "<wm_laterality_stats.py> Error: Input directory", args.inputDirectory, "does not exist."
+    exit()
+
+outdir = args.outputDirectory
+if not os.path.exists(outdir):
+    print "<wm_laterality_stats.py> Output directory", outdir, "does not exist, creating it."
+    os.makedirs(outdir)
+
+print ""
+print "--------------------------------------------------------------------"
+print "<wm_laterality_stats.py> Analyzing laterality results..."
+print "--------------------------------------------------------------------"
+print "input directory:\t", args.inputDirectory
+print "output directory:\t", args.outputDirectory
+
+input_mask = "{0}/*".format(args.inputDirectory);
+inputs = sorted(glob.glob(input_mask))
+input_dirs = list()
+for dir in inputs:
+    if os.path.isdir(dir):
+        test_file = os.path.join(dir, fname_to_test_if_output)
+        # test this is the output of laterality
+        if os.path.exists(test_file):
+            input_dirs.append(dir)
+            
+verbose = args.flag_verbose
+print "verbose:\t\t", verbose
 
 # Read groups file, in theory
 ###handedness = numpy.array([75, 85, 55, -35, -90, -85, -95, 70, -75, -25, 60, -70, 75, 100, 75, -35, -45, 90, 85, -75, 5, -80, 50, -40, -95, 100])
@@ -25,7 +81,78 @@ gender = numpy.array([1, 2, 1, 1, 2, 1, 1, 2, 2, 1, 1, 1, 1, 2, 1, 1, 1, 1, 2, 1
 IDs = numpy.array(['AG_5114', 'AG_5123', 'AG_5125', 'AG_5129', 'AG_5131', 'AG_5132', 'AG_5138', 'AG_5139', 'AG_5142', 'AG_5146', 'AG_5147', 'AG_5148', 'AG_5149', 'AG_5150', 'AG_5151', 'AG_5152', 'AG_5153', 'AG_5154', 'AG_5155', 'AG_5156', 'AG_5158', 'AG_5159', 'AG_5160', 'AG_5162', 'AG_5163', 'AG_5205', 'AG_5263', 'AG_5266', 'AG_5268']) 
 
 
-print IDs
+
+# test that IDs correspond to the input directories
+number_IDs = len(IDs)
+number_dirs = len(input_dirs)
+
+# test for matches
+match_IDs = numpy.empty(number_IDs,dtype=bool)
+match_dirs = numpy.empty(number_dirs,dtype=bool)
+match_IDs.fill(False)
+match_dirs.fill(False)
+for id_idx in range(number_IDs):
+    for dir_idx in range(number_dirs):
+        if IDs[id_idx] in input_dirs[dir_idx]:
+            match_IDs[id_idx] = True
+            match_dirs[dir_idx] = True
+
+if 0:
+    print "<wm_laterality_stats.py> Matched result:"
+    print match_IDs
+    print "<wm_laterality_stats.py> Matched IDs:"
+    print IDs[match_IDs]
+    print "<wm_laterality_stats.py> Data are missing for subject IDs:"
+    print IDs[~match_IDs]
+    print "<wm_laterality_stats.py> Matched directories:"
+    print numpy.array(input_dirs)[match_dirs]
+    print "<wm_laterality_stats.py> Extra directories that do not correspond to subject IDs:"
+    print numpy.array(input_dirs)[~match_dirs]
+
+
+if number_IDs == number_dirs:
+    print "<wm_laterality_stats.py> Number of subject IDs matches the number of directories."
+else:
+    print "--------------------------------------------------------------------"
+    print "<wm_laterality_stats.py> Warning: number of subject IDs:", number_IDs, "does not match the number of directories:", number_dirs
+    #print "<wm_laterality_stats.py> Input subject IDs:"
+    #print IDs
+    print "<wm_laterality_stats.py> Data directories are missing for subject IDs:"
+    print IDs[~match_IDs]
+    print "<wm_laterality_stats.py> Extra directories that do not correspond to subject IDs:"
+    print numpy.array(input_dirs)[~match_dirs]
+    print "--------------------------------------------------------------------"
+ 
+# there could be extra directories. Verify the matches are correct.
+number_IDs_matched = len(numpy.nonzero(match_IDs)[0])
+number_dirs_matched = len(numpy.nonzero(match_dirs)[0])
+
+# grab only the matched ones
+IDs = IDs[match_IDs]
+input_dirs = numpy.array(input_dirs)[match_dirs]
+handedness = handedness[match_IDs]
+gender = gender[match_IDs]
+
+print "--------------------------------------------------------------------"
+print "<wm_laterality_stats.py> Subject IDs and matching directories for analysis:"
+print "--------------------------------------------------------------------"
+found_error = False
+if number_IDs_matched == number_dirs_matched:
+    # test they are in the same order, perfectly matched
+    for id, dir in zip(IDs, input_dirs):
+        if id in dir:
+            print id, ":", dir
+        else:
+            print "<wm_laterality_stats.py> Error in subject ID and input list.", id, ":", dir
+            found_error = True
+else:
+    print "<wm_laterality_stats.py> Error in matching subjects with input directories. Potential duplicate IDs or directories."
+    found_error = True
+print "--------------------------------------------------------------------"
+if found_error:
+    print "<wm_laterality_stats.py> Please check your subject ID list against the subject directories within the input directory."
+    raise
+
 
 def plot_box(data, labels, fname):
     f = plt.figure()
@@ -39,20 +166,25 @@ def plot_box(data, labels, fname):
 
 data = []
 # Read data, and check if the data are all present
-for sidx in range(0,len(inputDirs)):
-
-    dir = inputDirs[sidx]
+print "<wm_laterality_stats.py> Reading data..."
+for sidx in range(0,len(input_dirs)):
+    dir = input_dirs[sidx]
+    if verbose:
+        print sidx, "/", len(input_dirs), ":", dir
     try:
         resultIO = wma.io.LateralityResults()
         resultIO.read(dir)
         data.append(resultIO)
     except:
-        print "unexpected error" , dir
-    else:
-        print "Read success", dir
-        print resultIO.laterality_index[0]
+        print "<wm_laterality_stats.py>  Unexpected error reading data directory:" , dir
+        raise
+
+
+# change to the output directory to save things there
+os.chdir(outdir)
 
 # compute summary stats
+print "<wm_laterality_stats.py> Computing summary stats..."
 median = numpy.zeros(len(data))
 iqr = numpy.zeros(len(data))
 skew = numpy.zeros(len(data))
@@ -65,14 +197,19 @@ num_fibers_Rhem = numpy.zeros(len(data))
 num_fibers_comm = numpy.zeros(len(data))
 
 for sidx in range(0,len(data)):
-    print sidx
-    print data[sidx].directory
+    if verbose:
+        print sidx, "/", len(data), ":", data[sidx].directory
     LI = data[sidx].laterality_index
     confidence = numpy.minimum(data[sidx].left_hem_similarity, data[sidx].right_hem_similarity)
     confidence_NZ = confidence[numpy.nonzero(LI)]
-    LI_NZ = LI[numpy.nonzero(LI)]
+    # analyze only the LI from hemispheric fibers, as commissural fibers have theirs set to 0.0
+    mask_hem = data[sidx].hemisphere != 0
+    LI_NZ = LI[mask_hem]
+    #LI_NZ = LI[numpy.nonzero(LI)]
     plt.figure()
     plt.plot(confidence_NZ, LI_NZ, 'o')
+    plt.savefig('confidence_vs_LI'+str(sidx)+'.pdf')
+    plt.close()
     #mode = scipy.stats.mode(numpy.array(LI_NZ*1000).astype(int))[0]/1000
     #print mode
     #print numpy.median(LI_NZ)
@@ -86,7 +223,7 @@ for sidx in range(0,len(data)):
     trimmed_LI_NZ = LI_NZ[select]
     #LI_NZ = trimmed_LI_NZ 
     median[sidx] = numpy.median(LI_NZ)
-    print sidx, ":" , median[sidx]
+    #print sidx, ":" , median[sidx]
     iqr[sidx] = scipy.stats.scoreatpercentile(LI_NZ, 75) - scipy.stats.scoreatpercentile(LI_NZ, 25)
     skew[sidx] = scipy.stats.skew(LI_NZ)
     trimmed_skew[sidx] = scipy.stats.skew(trimmed_LI_NZ)
@@ -99,31 +236,27 @@ for sidx in range(0,len(data)):
     num_fibers_Lhem[sidx] = len(numpy.nonzero(numpy.array(data[sidx].hemisphere == -1))[0])
     
 num_fibers_comm =  num_fibers - num_fibers_hem
-    
+
+print "<wm_laterality_stats.py> Computing histograms..."
 histograms = list()
 for sidx in range(0,len(data)) :
-    print sidx
+    if verbose:
+        print sidx, "/", len(data)
     LI = data[sidx].laterality_index
-    LI_NZ = LI[numpy.nonzero(LI)]
+    # analyze only the LI from hemispheric fibers, as commissural fibers have theirs set to 0.0
+    mask_hem = data[sidx].hemisphere != 0
+    LI_NZ = LI[mask_hem]
     #trimmed_LI_NZ = numpy.sort(LI_NZ)[50:-50]
     #LI_NZ = trimmed_LI_NZ
     #ret = plt.hist(LI_NZ,range=[-1,1],normed=True,bins=20)
     #ret = plt.hist(LI_NZ,range=[-1,1],normed=True,bins=60)
     ret = plt.hist(LI_NZ,range=[-1,1],normed=True,bins=40)
     histograms.append(ret[0])
-    bins = ret[1]
+    #bins = ret[1]
     plt.close()
 
-    
 
-# still need to re-run entire pipeline with optimal parameters and max # fibers possible
-# if need to exclude
-#analyze = (IDs != 'AG_5159') & (IDs != 'AG_5156')
-#gender = gender[numpy.nonzero(analyze)]
-#handedness = handedness[numpy.nonzero(analyze)]
-
-#gL = numpy.nonzero(handedness < 0)[0]
-#gR = numpy.nonzero(handedness >= 0)[0]
+print "<wm_laterality_stats.py> Making pdf figures..."
 
 # --------------
 threshold = 60
@@ -328,7 +461,7 @@ plt.plot(hist_sum,'r')
 plt.title('Left-Right')
 plt.legend(['L', 'R'])
 plt.savefig('leftplusright_hist.pdf')
-
+plt.close()
 
 plt.figure()
 hist_sum = numpy.zeros(len(histograms[0]))
@@ -383,6 +516,7 @@ for var in varlist:
     plt.plot(handedness,xr)
     plt.title(var[1])
     plt.savefig('handedness'+var[1]+'.pdf')
+    plt.close()
     r,p = scipy.stats.pearsonr(handedness, var[0])
     print var[1],".. r:", r, " p: ", p
 
