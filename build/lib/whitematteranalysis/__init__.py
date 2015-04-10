13d869c8b259d15e6ad148ee86bfa52cc369d6dc
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2015-04-10 12:28:23 -0400

    Undo accidental commit of build directory.

--- build/lib/whitematteranalysis/__init__.py
+++
@@ -1,5 +0,0 @@
-print "Unmodified wma from github"
-for m in ["io", "fibers", "filter", "laterality", "render", "cluster", "relative_distance", "congeal", "registration_functions", "mrml"]:
-  #print "importing ", m
-  exec 'import %s'%m
-
