6008b45b1cd5f54079df381532450b8cf7609bbe
Lauren J. O'Donnell <odonnell@bwh.harvard.edu>
2015-04-25 18:40:04 -0400

    BUG: fix command line to correspond to checked in parameter name

--- tests/test_wm_cluster_atlas.sh
+++ tests/test_wm_cluster_atlas.sh
@@ -2,7 +2,7 @@
 echo "<test> Running clustering test:"
 
 # turn off outlier rejection and positive definite approx for consistency with test results from before
-python -m cProfile -o profile.txt ../bin/wm_cluster_atlas.py test_data_small output/test_cluster_atlas -nystrom_sample 100 -advanced_only_testing_on -advanced_only_random_seed 10 -outlier_std 1000 -flag_pos_def_off
+python -m cProfile -o profile.txt ../bin/wm_cluster_atlas.py test_data_small output/test_cluster_atlas -nystrom_sample 100 -advanced_only_testing_on -advanced_only_random_seed 10 -outlier_std 1000 -advanced_only_force_pos_def_off
 
 echo "<test> cProfile run statistics:"
 python view_profile_stats.py
