1d64d6416dcab133cc2e0254202d7dc7c3550921
Lauren J. O'Donnell <odonnell@bwh.harvard.edu>
2015-04-25 12:15:48 -0400

    More informative import message

--- whitematteranalysis/__init__.py
+++ whitematteranalysis/__init__.py
@@ -1,4 +1,4 @@
-print "Unmodified wma from github"
+print "Importing whitematteranalysis package."
 for m in ["io", "fibers", "filter", "laterality", "render", "cluster", "relative_distance", "congeal", "registration_functions", "mrml"]:
   #print "importing ", m
   exec 'import %s'%m
