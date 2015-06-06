13f0a872f8dfc78f6ab1e6e58cbcbca3ea719235
Jon Pipitone <jon@pipitone.ca>
2015-06-06 08:14:38 -0400

    Added bin scripts to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,25 @@ setup(
     description='Processing of whole-brain streamline tractography.',
     long_description=open('README.md').read(),
     ext_modules = cythonize("whitematteranalysis/*.pyx"),
+    scripts = [ 
+        'bin/convert_ascii_vtk_to_binary.py',
+        'bin/picktracts_converter.py',
+        'bin/wm_append_all.py',
+        'bin/wm_cluster_atlas.py',
+        'bin/wm_cluster_from_atlas.py',
+        'bin/wm_cluster_subject.py',
+        'bin/wm_extract_cluster.py',
+        'bin/wm_laterality_all.py',
+        'bin/wm_mask_lines.py',
+        'bin/wm_measure_all_clusters.py',
+        'bin/wm_preprocess_all.py',
+        'bin/wm_quality_control_tractography.py',
+        'bin/wm_register_multisubject_faster.py',
+        'bin/wm_register_multisubject.py',
+        'bin/wm_register_to_atlas_new.py',
+        'bin/wm_register_to_atlas.py',
+        'bin/wm_separate_clusters_by_hemisphere.py',
+    ],
     include_dirs=[numpy.get_include()]
 )
 
