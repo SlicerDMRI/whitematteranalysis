39f0422611d50524de1a7562bafed06697722a80
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2015-06-25 16:18:12 -0400

    add python at top of file. save MRML scene file for easier loading of output clusters.

--- bin/wm_extract_cluster.py
+++ bin/wm_extract_cluster.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python
+print "wm_extract_cluster.py importing packages"
+
 import glob
 import argparse
 import os
 import shutil
+import numpy
+import whitematteranalysis as wma
 
 parser = argparse.ArgumentParser(
     description="Grab one cluster from within all subject directories and rename to include subject ID. Output all clusters into the output directory",
@@ -36,21 +41,31 @@ if not os.path.exists(outdir):
 
 # cluster filename we want
 fname_c = 'cluster_{0:05d}.vtp'.format(args.cluster)
+# output MRML filename
+fname_mrml = 'all_clusters_{0:05d}.mrml'.format(args.cluster)
+fname_mrml = os.path.join(outdir, fname_mrml)
 
 # Find input directories that may contain clusters
 input_mask = "{0}/*".format(args.inputDirectory)
 input_directories = sorted(glob.glob(input_mask))
 
-#print input_directories
-
 # Loop over inputs and try to find clusters
+fname_list = list()
 for dir in input_directories:
     if os.path.isdir(dir):
         subject_id = os.path.basename(dir)
         print dir
         fname = os.path.join(dir, fname_c)
         if os.path.exists(fname):
-            fname2 = os.path.join(outdir, subject_id+'_'+fname_c)
+            fname1 = subject_id+'_'+fname_c
+            fname_list.append(fname1)
+            fname2 = os.path.join(outdir, fname1)
             print fname, "===>>>>", fname2
             shutil.copy(fname, fname2)
+
+# also output a MRML file to load them all into Slicer
+# for now, random colors
+colors = numpy.random.random((len(fname_list),3))*255.0
+wma.mrml.write(fname_list, colors, fname_mrml, ratio = 1.0)
+
 
