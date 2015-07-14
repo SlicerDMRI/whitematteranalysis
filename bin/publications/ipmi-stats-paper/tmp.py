2257aa57269bac0219cc97c870fa0ef8778b09b5
Lauren O'Donnell <odonnell@bwh.harvard.edu>
2015-07-14 05:08:29 -0400

    Remove old/unused/deprecated scripts.

--- bin/publications/ipmi-stats-paper/tmp.py
+++
@@ -1,9 +0,0 @@
-import vtk
-import whitematteranalysis as wma
-pd = wma.io.read_polydata('white_matter_0000.vtk')
-pd2 = wma.filter.downsample(pd, 50)
-pd2 = wma.filter.downsample(pd, 50, preserve_point_data=True)
-
-pd2.GetPointData().SetActiveScalars('FA')
-
-ren = wma.render.render(pd2, scalar_bar=True, scalar_range=[0,1], data_mode="Point")
