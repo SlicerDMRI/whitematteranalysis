f42e63e0f0f62d770b6d711bb292cfdc41eb5028
zhangfanmark <zhangfanmark@gmail.com>
2016-02-11 23:16:11 -0500

    Add test cases and separate measurement matrix

--- tests/test_tract_measurment.py
+++ tests/test_tract_measurment.py
@@ -1,34 +1,66 @@
 import whitematteranalysis as wma
+import numpy
+import scipy.stats
+#---------------------------
+# Usage of tract_measurement
+#---------------------------
 
-##
+print '====================='
 print 'test 1: Column Comma'
 measurement_file = "./test_data_measurement/column_comma.txt"
 hierarchy = "Column"
 separator = "Comma"
-matrix = wma.tract_measurement.load_measurement(measurement_file, hierarchy, separator)
-print matrix
+tm = wma.tract_measurement.load_measurement(measurement_file, hierarchy, separator)
+
+print ' <tm> Extracted measurements:\n', tm.measurement_header
+print ' <tm> Number of clusters:', tm.cluster_number
+print ' <tm> Cluster ploydata path:', tm.cluster_path
+print ' <tm> Measurement matrix: shape', tm.measurement_matrix.shape
+print '      Each row gives the different measurements from each cluster.'
+print '      Each column gives certain measurement from all clusters.'
+print '      Examples:'
+print '      # Measurements from Cluster 0', tm.cluster_path[0], 'are (first 4):', tm.measurement_matrix[0, 0:4]
+print '      # Measurements of', tm.measurement_header[0], 'from all clusters are', tm.get_measurements_by_index(0)
+print '      # Mean value of', tm.measurement_header[2], 'of all clusters is', numpy.mean(tm.get_measurements_by_index(2))
+
 
 ##
+print '====================='
 print 'test 2: Column Tab'
 measurement_file = "./test_data_measurement/column_tab.txt"
 hierarchy = "Column"
 separator = "Tab"
-matrix = wma.tract_measurement.load_measurement(measurement_file, hierarchy, separator)
-print matrix
+tm = wma.tract_measurement.load_measurement(measurement_file, hierarchy, separator)
+
+print ' <tm> Extracted measurements:\n', tm.measurement_header
+print ' <tm> Number of clusters:', tm.cluster_number
+print ' <tm> Cluster ploydata path:', tm.cluster_path
+print ' <tm> Measurement matrix: shape', tm.measurement_matrix.shape
+print '      Each row gives the different measurements from each cluster.'
+print '      Each column gives certain measurement from all clusters.'
+print '      Examples:'
+print '      # Measurements from Cluster 0', tm.cluster_path[0], 'are (first 4)', tm.measurement_matrix[0, 0:4]
+print '      # Measurements of', tm.measurement_header[0], 'from all clusters are', tm.get_measurements_by_index(0)
+print '      # Mean value of', tm.measurement_header[2], 'of all clusters is', numpy.mean(tm.get_measurements_by_index(2))
+
 
 ##
-print 'test 3: Column Space'
-measurement_file = "./test_data_measurement/column_space.txt"
-hierarchy = "Column"
-separator = "Space"
-matrix = wma.tract_measurement.load_measurement(measurement_file, hierarchy, separator)
-print matrix
-
-##
-print 'test 4: multi subjects'
-measurement_folder = '/Users/fan/Desktop/tmp' # change path for testing
+print '====================='
+print 'test 3: multi subjects'
+measurement_folder = '/Users/fan/Desktop/tmp' # TODO: change data path, containing multiple measurement files
 hierarchy = "Column"
 separator = "Comma"
+
 measurement_list = wma.tract_measurement.load_measurement_in_folder(measurement_folder, hierarchy, separator)
-print len(measurement_list)
-print measurement_list[0]
+
+print ' <tm> Extract FA distribution of cluster 0 across all subjects'
+vec_FA = []
+for tm in measurement_list:
+    vec_FA.append(tm.get_measurements_by_name('tensors.FractionalAnisotropyMean')[0]) # TODO: change the name according to 'measurement_header'
+
+print ' <tm> FA vector length:', len(vec_FA)
+print ' <tm> FA vector (first 5):', vec_FA[0:5]
+print ' <tm> Mean FA of cluster 0 from all subjects:', numpy.mean(vec_FA)
+print ' <tm> Perform t-test between the first 10 subjects and the second 10 subjects'
+t, p = scipy.stats.ttest_ind(vec_FA[0:9], vec_FA[10:19], equal_var=False) # TODO: provide indices of different groups (e.g., patient V.S. control)
+print ' <tm> t =', t, ', p =', p
--- whitematteranalysis/tract_measurement.py
+++ whitematteranalysis/tract_measurement.py
@@ -1,15 +1,20 @@
 import csv
 import os
 import glob
+import numpy
 
 class TractMeasurement:
     """Fiber tract scalar measurement obtained from Slicer module FiberTractScalarMeasurement."""
 
     def __init__(self):
         self.measurement_file = None
+        self.cluster_number = None
+        self.cluster_path = None
+        self.measurement_header = None
         self.measurement_matrix = None
         self.hierarchy = 'Column'
         self.separator = 'Tab'
+
 
     def load(self):
         if not os.path.isfile(self.measurement_file):
@@ -32,26 +37,47 @@ class TractMeasurement:
             print "<tract_measurement.py> Error: Hierarchy should be one of Row or Column. "
             raise AssertionError
 
-        self.measurement_matrix = [];
+        txt_matrix = []
         with open(self.measurement_file, 'r') as txtfile:
             reader = csv.reader(txtfile, delimiter=separator_char, skipinitialspace=True,  quoting=csv.QUOTE_NONE)
             for row in reader:
                 row = map(str.strip, row)
-                self.measurement_matrix.append(row)
+                txt_matrix.append(row)
 
         if self.hierarchy == 'Row':
             # TODO: transfer Row output into list
             print "<tract_measurement.py> Error: Only support Column currently. "
             raise AssertionError
 
+        tmp_matrix = numpy.array(txt_matrix)
+
+        self.cluster_path = tmp_matrix[1:, 0]
+        self.measurement_header = tmp_matrix[0, 1:]
+        self.measurement_matrix = tmp_matrix[1:, 1:].astype(numpy.float)
+        self.cluster_number = self.measurement_matrix.shape[0]
+
     def check(self):
         # Simple check if the first three fields are Name, Num_Point and Num_Fiber
-        measures = self.measurement_matrix[0]
-        if measures[0] != 'Name' or measures[1] != 'Num_Points' or measures[2] != 'Num_Fibers':
+        header = self.measurement_header
+        if header[0] != 'Num_Points' or header[1] != 'Num_Fibers':
             print "<tract_measurement.py> Error: Measurement loading failed. First three fields extracted are: \n 1. ", measures[0], "\n 2. ", measures[1], "\n 3. ", measures[2], "\nwhich are expected to be \n 1. Name \n 2. Num_Points \n 3. Num_Fibers. "
             raise AssertionError
 
-        
+    def get_measurements_by_name(self, query_header_name):
+        if not numpy.sum(self.measurement_header == query_header_name) == 1:
+            print " Error: Header", query_header_name, "cannot be found. Select from:\n", self.measurement_header
+            return None
+
+        header_index = numpy.where(self.measurement_header == query_header_name)[0][0]
+        return self.measurement_matrix[:, header_index]
+
+    def get_measurements_by_index(self, query_index):
+        if query_index >= self.measurement_header.shape[0]:
+            print " Error: Index", query_index, "should range from 0 to", self.measurement_header.shape[0]
+
+        return self.measurement_matrix[:, query_index]
+
+
 def load_measurement(measurement_file, hierarchy = 'Column', separator = 'Tab'):
     """ Load measurement for one subject
     """
@@ -64,7 +90,7 @@ def load_measurement(measurement_file, hierarchy = 'Column', separator = 'Tab'):
     tm.load()
     tm.check()
 
-    return tm.measurement_matrix
+    return tm
 
 def load_measurement_in_folder(measurement_folder, hierarchy = 'Column', separator = 'Tab'):
     """ Load measurements for multiple subjects
