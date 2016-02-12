d15cc7f31c8081eb3b0df513992c7008f3bdf9b1
zhangfanmark <zhangfanmark@gmail.com>
2016-02-11 23:20:42 -0500

    Minor updates

--- tests/test_tract_measurment.py
+++ tests/test_tract_measurment.py
@@ -19,7 +19,7 @@ print ' <tm> Measurement matrix: shape', tm.measurement_matrix.shape
 print '      Each row gives the different measurements from each cluster.'
 print '      Each column gives certain measurement from all clusters.'
 print '      Examples:'
-print '      # Measurements from Cluster 0', tm.cluster_path[0], 'are (first 4):', tm.measurement_matrix[0, 0:4]
+print '      # Measurements from Cluster 0', tm.cluster_path[0], 'are (first 4):', tm.measurement_matrix[0, 0:3]
 print '      # Measurements of', tm.measurement_header[0], 'from all clusters are', tm.get_measurements_by_index(0)
 print '      # Mean value of', tm.measurement_header[2], 'of all clusters is', numpy.mean(tm.get_measurements_by_index(2))
 
@@ -39,7 +39,7 @@ print ' <tm> Measurement matrix: shape', tm.measurement_matrix.shape
 print '      Each row gives the different measurements from each cluster.'
 print '      Each column gives certain measurement from all clusters.'
 print '      Examples:'
-print '      # Measurements from Cluster 0', tm.cluster_path[0], 'are (first 4)', tm.measurement_matrix[0, 0:4]
+print '      # Measurements from Cluster 0', tm.cluster_path[0], 'are (first 4)', tm.measurement_matrix[0, 0:3]
 print '      # Measurements of', tm.measurement_header[0], 'from all clusters are', tm.get_measurements_by_index(0)
 print '      # Mean value of', tm.measurement_header[2], 'of all clusters is', numpy.mean(tm.get_measurements_by_index(2))
 
@@ -59,7 +59,7 @@ for tm in measurement_list:
     vec_FA.append(tm.get_measurements_by_name('tensors.FractionalAnisotropyMean')[0]) # TODO: change the name according to 'measurement_header'
 
 print ' <tm> FA vector length:', len(vec_FA)
-print ' <tm> FA vector (first 5):', vec_FA[0:5]
+print ' <tm> FA vector (first 5):', vec_FA[0:4]
 print ' <tm> Mean FA of cluster 0 from all subjects:', numpy.mean(vec_FA)
 print ' <tm> Perform t-test between the first 10 subjects and the second 10 subjects'
 t, p = scipy.stats.ttest_ind(vec_FA[0:9], vec_FA[10:19], equal_var=False) # TODO: provide indices of different groups (e.g., patient V.S. control)
