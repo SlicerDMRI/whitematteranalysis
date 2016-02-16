b45b0cead0b15c49416449467d95e0668bd732bc
Lauren J. O'Donnell <odonnell@bwh.harvard.edu>
2016-02-16 15:27:49 -0500

    update so test runs with current code

--- tests/test_wm_cluster_atlas.sh
+++ tests/test_wm_cluster_atlas.sh
@@ -2,8 +2,8 @@
 echo "<test> Running clustering test:"
 
 # turn off outlier rejection and positive definite approx for consistency with test results from before
-python -m cProfile -o profile.txt ../bin/wm_cluster_atlas.py test_data_small output/test_cluster_atlas -nystrom_sample 100 -advanced_only_testing_on -advanced_only_random_seed 10 -outlier_std 1000 -advanced_only_force_pos_def_off
+python -m cProfile -o profile.txt ../bin/wm_cluster_atlas.py test_data_small output/test_cluster_atlas -nystrom_sample 100 -advanced_only_testing_on -advanced_only_random_seed 10 -cluster_outlier_std 1000 -iter 1 -advanced_only_force_pos_def_off -l 0 -f 500
 
 echo "<test> cProfile run statistics:"
-python view_profile_stats.py
+python view_profile_stats.py  profile.txt
 
