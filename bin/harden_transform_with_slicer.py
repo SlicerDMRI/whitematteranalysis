ad83f3bce8ab3a8c85ae42575d9461f9b8c83402
zhangfanmark <zhangfanmark@gmail.com>
2016-05-18 14:28:29 -0400

    initialization

---
+++ bin/harden_transform_with_slicer.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import argparse
+import slicer
+import os
+
+parser = argparse.ArgumentParser(
+    description="Harden transform with Slicer.",
+    epilog="Written by Fan Zhang, fzhang@bwh.harvard.edu")
+
+parser.add_argument("-v", "--version",
+    action="version", default=argparse.SUPPRESS,
+    version='1.0',
+    help="Show program's version number and exit")
+parser.add_argument(
+    'polydata',
+    help='')
+parser.add_argument(
+    'transform',
+    help='')
+parser.add_argument(
+    'inverse',
+    help='')
+parser.add_argument(
+    'outdir',
+    help='')
+
+args = parser.parse_args()
+
+def harden_transform(polydata, transform, inverse, outdir):
+
+    check_load, polydata_node = slicer.util.loadModel(str(polydata), 1)
+    if not check_load:
+        print 'Could not load polydata file:', polydata
+        return
+
+    check_load, transform_node = slicer.util.loadTransform(str(transform), 1)
+    if not check_load:
+        print 'Could not load transform file:', transform
+        return
+
+    if inverse == "1":
+        transform_node.Inverse()
+
+    logic = slicer.vtkSlicerTransformLogic()
+    t_node_id = transform_node.GetID()
+
+    # harden transform
+    polydata_node.SetAndObserveTransformNodeID(t_node_id)
+    logic.hardenTransform(polydata_node)
+
+    polydata_base_path, polydata_name = os.path.split(polydata)
+    output_name = polydata_name.replace(".", "_trans.")
+    if inverse == "1":
+        output_name = polydata_name.replace(".", "_inv_trans.")
+    slicer.util.saveNode(polydata_node, os.path.join(outdir, output_name))
+
+harden_transform(args.polydata, args.transform, args.inverse, args.outdir)
---
+++ bin/wm_harden_transform.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+import argparse
+import os
+from joblib import Parallel, delayed
+
+try:
+    import whitematteranalysis as wma
+except:
+    print "<wm_harden_transform_with_slicer> Error importing white matter analysis package\n"
+    raise
+
+#-----------------
+# Parse arguments
+#-----------------
+parser = argparse.ArgumentParser(
+    description="Harden transform with Slicer.",
+    epilog="Written by Fan Zhang, fzhang@bwh.harvard.edu")
+
+parser.add_argument("-v", "--version",
+    action="version", default=argparse.SUPPRESS,
+    version='1.0',
+    help="Show program's version number and exit")
+parser.add_argument(
+    'inputDirectory',
+    help='Directory of VTK/VTP files that are going to be transformed.')
+parser.add_argument(
+    'outputDirectory',
+    help='Directory of the transformed results.')
+parser.add_argument(
+    'Slicer',
+    help='Path of 3D Slicer.')
+parser.add_argument(
+    '-t', dest="transform_file",
+    help='Individual transform matrix file. If this is assigned, all input files will be transformed with the same transform matrix.')
+parser.add_argument(
+    '-ts', dest="transform_folder",
+    help='Multiple transform matrix files. If this is assigned, each input file will be transformed with one corresponding transform matrix. Make sure that the file names of the TPK/VTP file and the transform file match each other in alphabetical order.')
+parser.add_argument(
+    '-i', action = 'store_true', dest = "inverse_transform",
+    help='Transform with the inverse transform matrix if given.')
+parser.add_argument(
+    '-j', action="store", dest="numberOfJobs", type=int,
+    help='Number of processors to use.')
+
+args = parser.parse_args()
+
+
+if not os.path.isdir(args.inputDirectory):
+    print "Error: Input directory", args.inputDirectory, "does not exist."
+    exit()
+
+outdir = args.outputDirectory
+if not os.path.exists(outdir):
+    print "Output directory", outdir, "does not exist, creating it."
+    os.makedirs(outdir)
+
+slicer_path = args.Slicer
+if not os.path.exists(slicer_path):
+    print "Error: 3D Slicer", args.Slicer, "does not exist."
+    exit()
+
+if (args.transform_file is None and args.transform_folder is None) or \
+    (args.transform_file is not None and args.transform_folder is not None):
+    print "Error: Either -t or -ts needs be provided."
+    exit()
+elif args.transform_file is not None:
+    transform_way = 'individual'
+    transform_path = args.transform_file
+    if not os.path.isfile(args.transform_file):
+        print "Error: Input transform file", args.transform_file, "does not exist or it is not a file."
+        exit()
+elif args.transform_folder is not None:
+    transform_way = 'multiple'
+    transform_path = args.transform_folder
+    if not os.path.isdir(args.transform_folder):
+        print "Error: Input transform file folder ", args.transform_folder, "does not exist."
+        exit()
+
+inverse = args.inverse_transform
+
+if args.numberOfJobs is not None:
+    number_of_jobs = args.numberOfJobs
+else:
+    number_of_jobs = 1
+
+input_polydatas = wma.io.list_vtk_files(args.inputDirectory)
+number_of_polydatas = len(input_polydatas)
+
+if transform_way == 'multiple':
+    input_transforms = wma.io.list_transform_files(args.transform_folder)
+    number_of_transforms = len(input_transforms)
+    if number_of_polydatas != number_of_transforms:
+        print "Error: The number of input VTK/VTP files", number_of_polydatas,"should be equal to the number of transform files", number_of_transforms
+        exit()
+    for polydata, transform in zip(input_polydatas, input_transforms):
+        print "<wm_harden_transform_with_slicer>", transform, 'is applied to', polydata
+
+print "<wm_harden_transform_with_slicer> Starting scalar measurement extraction."
+print ""
+print "=====input directory======\n", args.inputDirectory
+print "=====output directory=====\n", args.outputDirectory
+print "=====3D Slicer====\n", args.Slicer
+print "=====Way of transform====\n", transform_way
+print "=====Inverse? ====\n", inverse
+print "=====Transform file(s) path====\n", transform_path
+print "=====Number of jobs:====\n", number_of_jobs
+
+def command_harden_transform(polydata, transform, inverse, slicer_path, outdir):
+    if inverse:
+        str_inverse = 1
+    else:
+        str_inverse = 0
+
+    print "<wm_harden_transform_with_slicer> Transforming:", polydata
+    cmd = slicer_path + " --no-main-window --python-script $(which harden_transform_with_slicer.py) " + \
+          polydata + " " + transform + " " + str(str_inverse) + " " + outdir + " --exit-after-startup" + \
+          ' >> ' + os.path.join(args.outputDirectory, 'log.txt 2>&1')
+    os.system(cmd)
+
+if transform_way == 'multiple':
+    for polydata, transform in zip(input_polydatas, input_transforms):
+        print "======", transform, ' <TO> ', polydata
+    print "\n"
+    Parallel(n_jobs=number_of_jobs, verbose=1)(
+        delayed(command_harden_transform)(polydata, transform, inverse, slicer_path, outdir)
+        for polydata, transform in zip(input_polydatas, input_transforms))
+else:
+    print "======", transform_path, "will be applied to all inputs.\n"
+
+    Parallel(n_jobs=number_of_jobs, verbose=1)(
+        delayed(command_harden_transform)(polydata, transform_path, inverse, slicer_path, outdir)
+        for polydata in input_polydatas)
+
+output_polydatas = wma.io.list_vtk_files(outdir)
+number_of_results = len(output_polydatas)
+print "<wm_harden_transform_with_slicer> Transform were conducted for", number_of_results, "subjects."
+
+if number_of_results != number_of_polydatas:
+    print "Error: The numbers of inputs and outputs are different. Check log file for errors."
+else:
+    os.remove(os.path.join(args.outputDirectory, 'log.txt'))
