53c7506ff89f3ceb6e136908ae7b7184e473fcc7
Fan <fzhang@bwh.harvard.edu>
2019-06-26 13:41:19 -0400

    Update wm_download_atlas

--- bin/wm_download_anatomically_curated_atlas.py
+++ bin/wm_download_anatomically_curated_atlas.py
@@ -2,7 +2,7 @@
 import os
 import argparse
 import urllib2
-import tarfile
+import zipfile
 import sys
 
 #-----------------
@@ -23,9 +23,9 @@ parser.add_argument(
 args = parser.parse_args()
 
 def download_file(url, output_file):
-    
+
     try:
-        print("Downloading: {0}".format(url))
+        print("* Downloading: {0}".format(url))
         url_file = urllib2.urlopen(url)
         meta = url_file.info()
         file_size = int(meta.getheaders("Content-Length")[0])
@@ -46,69 +46,57 @@ def download_file(url, output_file):
             sys.stdout.write(status)
 
         output.close()  
-        print ' '      
+        print ''      
     except:
-        print 'Download fail!! Please check your network connection and rerun the script.'
+        print '[ERRIR] Download fails! Please check your network connection and rerun the script.'
         print sys.exc_info()
         exit()
 
-def extract_requested_folder(members, sub_folder):
-
-    for tarinfo in members:
-        if sub_folder in tarinfo.name:
-            yield tarinfo
-
-def extract_atlas_from_archive(archive_file, output_dir, sub_folder=None, remove_after_extraction=False):
+def extract_from_zip(archive_file, output_dir, remove_after_extraction=False):
 
     try:
-        zip_ref = tarfile.open(archive_file)
-
-        if sub_folder is not None:
-            members = extract_requested_folder(zip_ref, sub_folder)
-            zip_ref.extractall(output_dir, members=members)
-        else:
-            zip_ref.extractall(output_dir)
-
+        zip_ref = zipfile.ZipFile(archive_file, 'r')
+        zip_ref.extractall(output_dir)
         zip_ref.close()
-
-        if remove_after_extraction is True:
-            os.remove(archive_file)
-
     except:
-        print 'Extraction fail!!'
+        print '[ERROR] Extraction fails!'
         print sys.exc_info()
         exit()
+
+    if remove_after_extraction:
+        os.remove(archive_file)
 
 outdir = os.path.abspath(args.outputDirectory)
 if not os.path.exists(args.outputDirectory):
-    print "Error: Output directory", args.outputDirectory, "does not exist, creating it."
+    print "<wm_download_org_atlas> Output directory", args.outputDirectory, "does not exist, creating it."
     os.makedirs(outdir)
 
 requested_atlas = args.requested_atlas
 
-repo = 'https://github.com/SlicerDMRI/ORG-Atlases'
-version = 'v1.0'
+repo = 'https://zenodo.org/record/2648292/'
+version = 'v1.1.1'
 
 org_atlases_version_folder_name = 'ORG-Atlases-' + version[1:]
 org_atlases_version_folder = os.path.join(outdir, org_atlases_version_folder_name)
-
-output_atlas_folder = os.path.join(org_atlases_version_folder, requested_atlas)
-if os.path.exists(output_atlas_folder):
-    print '\n<wm_download_org_atlas> Found an existing atlas at \''+output_atlas_folder+'\'. Remove this folder before redownload.'
-    print ''
-    exit()
-
-archive_url = repo + '/archive/' + version + '.tar.gz'
-downloaded_archive_file = os.path.join(outdir, version + '.tar.gz')
+if not os.path.exists(org_atlases_version_folder):
+    os.makedirs(org_atlases_version_folder)
 
 if requested_atlas == 'ORG-800FC-100HCP' or requested_atlas == 'ORG-2000FC-100HCP':
-    FC_atlas_url = repo + '/releases/download/' + version + '/' + requested_atlas + '.tar.gz'
-    REG_atlas_url = repo + '/releases/download/' + version + '/ORG-RegAtlas-100HCP.tar.gz'
+    FC_atlas_url = repo + 'files/' + requested_atlas + '.zip?download=1'
+    REG_atlas_url = repo + 'files/' + 'ORG-RegAtlas-100HCP.zip?download=1'
+
+    population_T1_url = repo + 'files/' + '100HCP-population-mean-T1.nii.gz?download=1'
+    population_T2_url = repo + 'files/' + '100HCP-population-mean-T2.nii.gz?download=1'
+    population_b0_url = repo + 'files/' + '100HCP-population-mean-b0.nii.gz?download=1'
     
-    downloaded_FC_atlas_file = os.path.join(org_atlases_version_folder, requested_atlas + '.tar.gz')
-    downloaded_Reg_atlas_file = os.path.join(org_atlases_version_folder, 'ORG-RegAtlas-100HCP.tar.gz')
+    downloaded_FC_atlas_file = os.path.join(org_atlases_version_folder, requested_atlas + '.zip')
+    downloaded_Reg_atlas_file = os.path.join(org_atlases_version_folder, 'ORG-RegAtlas-100HCP.zip')
+
+    downloaded_population_T1_file = os.path.join(org_atlases_version_folder, '100HCP-population-mean-T1.nii.gz')
+    downloaded_population_T2_file = os.path.join(org_atlases_version_folder, '100HCP-population-mean-T2.nii.gz')
+    downloaded_population_b0_file = os.path.join(org_atlases_version_folder, '100HCP-population-mean-b0.nii.gz')
 else:
-    print '<wm_download_org_atlas> ' + requested_atlas + 'is not available yet. Please check input.'
+    print '<wm_download_org_atlas> ' + requested_atlas + 'is not available. Please check input.'
     print ''
     exit()
 
@@ -118,32 +106,50 @@ print "===== Release version   : ", version
 print "===== Download from     : ", repo
 print "===== Output directory  : ", outdir
 print "===== Requested atlas   : ", requested_atlas
+print ""
 
 if requested_atlas == 'ORG-800FC-100HCP':
-    print '** The ' +requested_atlas+ ' atlas is an anatomically curated white matter atlas, with annotated anatomical label provided for each cluster. '\
+    print '<wm_download_org_atlas> The ' +requested_atlas+ ' atlas is an anatomically curated white matter atlas, with annotated anatomical label provided for each cluster. '\
            + 'MRML files that are used to organize fiber clusters that belong to the same anatomical fiber tract are provided in the atlas.' 
+    print ''
 elif requested_atlas == 'ORG-2000FC-100HCP':
-    print '** The ' +requested_atlas+ ' atlas is provided for applications that can be benefit from a fine scale white matter parcellation. This an uncurated white matter atlas.'
-
-
-print '<wm_download_org_atlas> Start downloading requested files...'
+    print '<wm_download_org_atlas> The ' +requested_atlas+ ' atlas is provided for applications that can be benefit from a fine scale white matter parcellation. This an uncurated white matter atlas.'
+    print ''
 
 if requested_atlas == 'ORG-800FC-100HCP' or requested_atlas == 'ORG-2000FC-100HCP':
-    print ' '
-    download_file(archive_url, downloaded_archive_file)
-    extract_atlas_from_archive(downloaded_archive_file, outdir, sub_folder=requested_atlas, remove_after_extraction=True)
-
+    print '<wm_download_org_atlas> Tractography registration atlas.'
     if not os.path.exists(os.path.join(org_atlases_version_folder, 'ORG-RegAtlas-100HCP/registration_atlas.vtk')):
-        print ' '
         download_file(REG_atlas_url, downloaded_Reg_atlas_file)
-        extract_atlas_from_archive(downloaded_Reg_atlas_file, org_atlases_version_folder, remove_after_extraction=True)
+        extract_from_zip(downloaded_Reg_atlas_file, org_atlases_version_folder, remove_after_extraction=True)
     else:
-        print ' '
-        print 'Registration atlas already donwloaded: ORG-RegAtlas-100HCP/registration_atlas.vtk'
+        print '<wm_download_org_atlas> Skip downloading: There is an existing registration atlas at \'ORG-RegAtlas-100HCP/registration_atlas.vtk\' in the output folder.'
+        print ''
 
-    print ' '
-    download_file(FC_atlas_url, downloaded_FC_atlas_file)
-    extract_atlas_from_archive(downloaded_FC_atlas_file, org_atlases_version_folder, remove_after_extraction=True)
+    print '<wm_download_org_atlas> Fiber clustering atlas.'
+    output_atlas_folder = os.path.join(org_atlases_version_folder, requested_atlas)
+    if not os.path.exists(output_atlas_folder):
+        download_file(FC_atlas_url, downloaded_FC_atlas_file)
+        extract_from_zip(downloaded_FC_atlas_file, org_atlases_version_folder, remove_after_extraction=True)
+    else:
+        print '<wm_download_org_atlas> Skip downloading: There is an existing fiber clustering atlas at \''+requested_atlas+'\' in the output folder.'
+        print ''
+
+    print '<wm_download_org_atlas> Population mean T1/T2/b0 images.'
+    if not os.path.exists(downloaded_population_T1_file):
+        download_file(population_T1_url, downloaded_population_T1_file)
+    else:
+        print '<wm_download_org_atlas> Skip downloading: There is an existing \'100HCP-population-mean-T1.nii.gz\' in the output folder.',
+        print ''
+    if not os.path.exists(downloaded_population_T2_file):
+        download_file(population_T2_url, downloaded_population_T2_file)
+    else:
+        print '<wm_download_org_atlas> Skip downloading: There is an existing \'100HCP-population-mean-T2.nii.gz\' in the output folder.',
+        print ''
+    if not os.path.exists(downloaded_population_b0_file):
+        download_file(population_b0_url, downloaded_population_b0_file)
+    else:
+        print '<wm_download_org_atlas> Skip downloading: There is an existing \'100HCP-population-mean-b0.nii.gz\' in the output folder.',
+        print ''
 
 print ''
 print '<wm_download_org_atlas> Successfully downloaded to', outdir
