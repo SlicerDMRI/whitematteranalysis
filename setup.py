7c71fb162e1948cd58186ae747c2d70ee6df4645
Fan <fzhang@bwh.harvard.edu>
2019-06-27 23:19:46 -0400

    File reorganize

--- setup.py
+++ setup.py
@@ -98,35 +98,8 @@ setup(
     cmdclass=LazyCommandClass(),
 
     scripts = [ 
-        'bin/picktracts_converter.py',
-        'bin/harden_transform_with_slicer.py',
-        'bin/wm_assess_cluster_location_by_hemisphere.py',
-        'bin/wm_batch.sh',
-        'bin/wm_diffusion_measurements.py',
-        'bin/wm_cluster_atlas.py',
-        'bin/wm_cluster_from_atlas.py',
-        'bin/wm_extract_cluster.py',
-        'bin/wm_laterality_all.py',
-        'bin/wm_preprocess_all.py',
-        'bin/wm_quality_control_tractography.py',
-        'bin/wm_register_multisubject_faster.py',
-        'bin/wm_register_to_atlas_new.py',
-        'bin/wm_separate_clusters_by_hemisphere.py',
-        'bin/wm_flatten_length_distribution.py',
-        'bin/wm_create_mrml_file.py',
-        'bin/wm_cluster_remove_outliers.py',
-        'bin/wm_measure_all_subjects.py',
-        'bin/wm_quality_control_cluster_measurements.py',
-        'bin/wm_statistics.py',
-        'bin/wm_statistics_export_data.py',
-        'bin/wm_quality_control_after_clustering.py',
-        'bin/wm_harden_transform.py',
-        'bin/wm_append_clusters.py',
-        'bin/wm_remove_data_along_tracts.py',
-        'bin/wm_measure_endpoint_overlap.py',
-        'bin/wm_extract_clusters_by_endpoints.py',
-        'bin/wm_assess_cluster_location.py',
-        'bin/wm_vtp2vtk.py',
-        'bin/wm_download_anatomically_curated_atlas.py'
+        'bin/*.py',
+        'bin/*.sh',
+        'utilities/*.py'
     ]
 )
