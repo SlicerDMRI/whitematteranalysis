b29a29cc1ed43d544481b77d5b0806c828bb0dce
zhangfanmark <fzhang@bwh.harvard.edu>
2019-07-11 13:23:59 -0400

    Stop the pipeline if one step is broken.

--- bin/wm_apply_ORG_atlas_to_subject.sh
+++ bin/wm_apply_ORG_atlas_to_subject.sh
@@ -156,13 +156,13 @@ caseID=${fn//.$ext/}
 
 reportMappingParameters
 
-echo "<WMA_batch> Working on input tractography:" $InputTractography
-echo "<WMA_batch> Case ID is assigned to be the file name of the input: [" ${caseID} "]"
+echo "<wm_apply_ORG_atlas_to_subject> Working on input tractography:" $InputTractography
+echo "<wm_apply_ORG_atlas_to_subject> Case ID is assigned to be the file name of the input: [" ${caseID} "]"
 echo ""
 
 # Setup output
 OutputCaseFolder=$OutputDir/${caseID}
-echo "<WMA_batch> Fiber clustering result will be stored at:" $OutputCaseFolder
+echo "<wm_apply_ORG_atlas_to_subject> Fiber clustering result will be stored at:" $OutputCaseFolder
 if [ ! -d $OutputCaseFolder ]; then
 	echo ' - create an output folder.' 
 	mkdir -p $OutputCaseFolder
@@ -172,10 +172,11 @@ else
 	numfiles=${#numfiles[@]}
 	if [ $numfiles -gt 1 ] ;then
 		echo ""
-		echo "** Final anantomical tracts ($numfiles tracts) exist in the output folder. Manually remove all files to rerun."
+		echo "** Anantomical tracts ($numfiles tracts) are detected in the output folder. Manually remove all files to rerun."
 		echo ""
-		exit
+		#exit
 	fi
+
 	echo ' - output folder exists.'
 
 fi
@@ -184,13 +185,13 @@ echo ""
 # Setup white matter parcellation atlas
 RegAtlasFolder=$AtlasBaseFolder/ORG-RegAtlas-100HCP
 FCAtlasFolder=$AtlasBaseFolder/ORG-800FC-100HCP
-echo "<WMA_batch> White matter atlas: " $AtlasBaseFolder
+echo "<wm_apply_ORG_atlas_to_subject> White matter atlas: " $AtlasBaseFolder
 echo " - tractography registration atlas:" $RegAtlasFolder
 echo " - fiber clustering atlas:" $FCAtlasFolder
 echo ""
 
 
-echo "<WMA_batch> Tractography registraion with mode [" $RegMode "]"
+echo "<wm_apply_ORG_atlas_to_subject> Tractography registraion with mode [" $RegMode "]"
 RegistrationFolder=$OutputCaseFolder/TractRegistration
 if [ "$RegMode" == "rig" ]; then
 	RegTractography=$RegistrationFolder/${caseID}/output_tractography/${caseID}_reg.vtk
@@ -218,13 +219,19 @@ elif [ "$RegMode" == "nonrig" ]; then
 fi
 echo ""
 
-echo $RegTractography
+if [ ! -f $RegTractography ]; then
+	echo ""
+	echo "ERROR: Tractography registration failed. The output registered tractography data can not be found."
+	echo ""
+	exit
+fi
+
 # Get the case ID for fiber clustering
 ext=${RegTractography#*.}
 fn=$(basename -- $RegTractography)
 FCcaseID=${fn//.$ext/}
 
-echo "<WMA_batch> Fiber clustering for whole-brain 800 fiber cluster parcellation."
+echo "<wm_apply_ORG_atlas_to_subject> Fiber clustering for whole-brain 800 fiber cluster parcellation."
 FiberClusteringInitialFolder=$OutputCaseFolder/FiberClustering/InitialClusters
 if [ ! -f $FiberClusteringInitialFolder/$FCcaseID/cluster_00800.vtp ]; then
 	wm_cluster_from_atlas.py -j $NumThreads \
@@ -234,7 +241,16 @@ else
 fi
 echo ""
 
-echo "<WMA_batch> Outlier fiber removal."
+numfiles=($FiberClusteringInitialFolder/$FCcaseID/*vtp)
+numfiles=${#numfiles[@]}
+if [ $numfiles -lt 800 ]; then
+	echo ""
+	echo "ERROR: Initial fiber clustering failed. There should be 800 resulting fiber clusters, but only $numfiles generated."
+	echo ""
+	exit
+fi
+ 
+echo "<wm_apply_ORG_atlas_to_subject> Outlier fiber removal."
 FiberClusteringOutlierRemFolder=$OutputCaseFolder/FiberClustering/OutlierRemovedClusters
 if [ ! -f $FiberClusteringOutlierRemFolder/${FCcaseID}_outlier_removed/cluster_00800.vtp ]; then
 	wm_cluster_remove_outliers.py \
@@ -244,7 +260,16 @@ else
 fi
 echo ""
 
-echo "<WMA_batch> Hemisphere location assessment in the atlas space."
+numfiles=($FiberClusteringOutlierRemFolder/${FCcaseID}_outlier_removed/*vtp)
+numfiles=${#numfiles[@]}
+if [ $numfiles -lt 800 ]; then
+	echo ""
+	echo "ERROR: Outlier removal failed. There should be 800 resulting fiber clusters, but only $numfiles generated."
+	echo ""
+	exit
+fi
+
+echo "<wm_apply_ORG_atlas_to_subject> Hemisphere location assessment in the atlas space."
 if [ ! -f $FiberClusteringOutlierRemFolder/${FCcaseID}_outlier_removed/cluster_location_by_hemisphere.log ]; then
 	wm_assess_cluster_location_by_hemisphere.py \
 		-clusterLocationFile $FCAtlasFolder/cluster_hemisphere_location.txt \
@@ -254,7 +279,14 @@ else
 fi
 echo ""
 
-echo "<WMA_batch> Transform fiber clusters back to input tractography space."
+if [ ! -f $FiberClusteringOutlierRemFolder/${FCcaseID}_outlier_removed/cluster_location_by_hemisphere.log ]; then
+	echo ""
+	echo "ERROR: Hemisphere location assessment failed. There should a cluster_location_by_hemisphere.log file, stating: \"<wm_assess_cluster_location_by_hemisphere.py> Done!!!\" "
+	echo ""
+	exit
+fi
+
+echo "<wm_apply_ORG_atlas_to_subject> Transform fiber clusters back to input tractography space."
 FiberClustersInTractographySpace=$OutputCaseFolder/FiberClustering/TransformedClusters/${caseID}
 if [ "$RegMode" == "rig" ]; then
 	
@@ -303,7 +335,16 @@ elif [ "$RegMode" == "nonrig" ]; then
 fi
 echo ""
 
-echo "<WMA_batch> Separate fiber clusters by hemisphere."
+numfiles=($FiberClustersInTractographySpace/*vtp)
+numfiles=${#numfiles[@]}
+if [ $numfiles -lt 800 ]; then
+	echo ""
+	echo "ERROR: Transforming fiber clusters failed. There should be 800 resulting fiber clusters, but only $numfiles generated."
+	echo ""
+	exit
+fi
+
+echo "<wm_apply_ORG_atlas_to_subject> Separate fiber clusters by hemisphere."
 SeparatedClustersFolder=$OutputCaseFolder/FiberClustering/SeparatedClusters
 if [ ! -f $SeparatedClustersFolder/tracts_commissural/cluster_00800.vtp ]; then
 
@@ -314,86 +355,96 @@ else
 fi
 echo ""
 
-if [ $DiffMeasure == 1 ]; then
+numfiles=($SeparatedClustersFolder/tracts_commissural/*vtp)
+numfiles=${#numfiles[@]}
+if [ $numfiles -lt 800 ]; then
+	echo ""
+	echo "ERROR: Separating fiber clusters failed. There should be 800 resulting fiber clusters in each folder, but only $numfiles generated."
+	echo ""
+	exit
+fi
 
-	echo "<WMA_batch> Report diffusion measurements of the fiber clusters."
-	if [ ! -f $SeparatedClustersFolder/tracts_commissural/DiffusionMeasurements.csv ]; then
-		wm_diffusion_measurements.py \
-			$SeparatedClustersFolder/tracts_commissural $SeparatedClustersFolder/tracts_commissural $FiberTractMeasurementsCLI
-	else
-		echo " - diffusion measurements of commissural clusters has been done."
-	fi
-	if [ ! -f $SeparatedClustersFolder/tracts_left_hemisphere/DiffusionMeasurements.csv ]; then
-		wm_diffusion_measurements.py \
-			$SeparatedClustersFolder/tracts_left_hemisphere $SeparatedClustersFolder/tracts_left_hemisphere $FiberTractMeasurementsCLI
-	else
-		echo " - diffusion measurements of left hemisphere clusters has been done."
-	fi
-	if [ ! -f $SeparatedClustersFolder/tracts_right_hemisphere/DiffusionMeasurements.csv ]; then
-		wm_diffusion_measurements.py \
-			$SeparatedClustersFolder/tracts_right_hemisphere $SeparatedClustersFolder/tracts_right_hemisphere $FiberTractMeasurementsCLI
-	else
-		echo " - diffusion measurements of right hemisphere clusters has been done."
-	fi
+echo "<wm_apply_ORG_atlas_to_subject> Append clusters into anatomical tracts."
+
+AnatomicalTractsFolder=$OutputCaseFolder/AnatomicalTracts
+if [ ! -f $AnatomicalTractsFolder/T_UF_right.vtp ]; then
+	
+	wm_append_clusters_to_anatomical_tracts.py $SeparatedClustersFolder/ $FCAtlasFolder $AnatomicalTractsFolder
+
+else
+	echo " - Appending clusters into anatomical tracts has been done."
 fi
 echo ""
 
-echo "<WMA_batch> Append clusters into anatomical tracts."
-
-hemispheric_tracts=(T_AF T_CB T_MdLF T_PLIC T_SLF-I T_SLF-II T_SLF-III T_EC T_EmC T_ICP T_ILF T_IOFF T_UF \
-	T_Intra-CBLM-I\&P T_Intra-CBLM-PaT \
-	T_CPC T_CR-F T_CR-P T_CST \
-	T_SF T_SO T_SP T_TF T_TO T_TP\
-	T_Sup-F T_Sup-FP T_Sup-O T_Sup-OT T_Sup-P T_Sup-PO T_Sup-PT T_Sup-T )
-
-commissural_tracts=(T_CC1 T_CC2 T_CC3 T_CC4 T_CC5 T_CC6 T_CC7 T_MCP )
-
-AnatomicalTractsFolder=$OutputCaseFolder/AnatomicalTracts
-
-for T_mrml in "${hemispheric_tracts[@]}"
-do 
-	tract_name=$(basename $T_mrml)
-	# echo " - Appending tract:" $tract_name
-	if [ ! -f $AnatomicalTractsFolder/${tract_name}_left.vtp ]; then
-		wm_append_clusters.py $SeparatedClustersFolder/tracts_left_hemisphere $AnatomicalTractsFolder \
-			-appendedTractName ${tract_name}_left -tractMRML $FCAtlasFolder/${tract_name}.mrml
-	fi
-	if [ ! -f $AnatomicalTractsFolder/${tract_name}_right.vtp ]; then
-		wm_append_clusters.py $SeparatedClustersFolder/tracts_right_hemisphere $AnatomicalTractsFolder \
-			-appendedTractName ${tract_name}_right -tractMRML $FCAtlasFolder/${tract_name}.mrml
-	fi
-done
-
-for T_mrml in "${commissural_tracts[@]}"
-do 
-	tract_name=$(basename $T_mrml)
-	# echo " - Appending tract:" $tract_name
-	if [ ! -f $AnatomicalTractsFolder/${tract_name}.vtp ]; then
-		wm_append_clusters.py $SeparatedClustersFolder/tracts_commissural $AnatomicalTractsFolder \
-			-appendedTractName ${tract_name} -tractMRML $FCAtlasFolder/${tract_name}.mrml
-	fi
-done
-echo ""
+numfiles=($AnatomicalTractsFolder/*vtp)
+numfiles=${#numfiles[@]}
+if [ $numfiles -lt 74 ]; then
+	echo ""
+	echo "ERROR: Appending clusters into anatomical tracts failed. There should be 74 resulting fiber clusters, but only $numfiles generated."
+	echo ""
+	exit
+fi
 
 if [ $DiffMeasure == 1 ]; then
 
-	echo "<WMA_batch> Report diffusion measurements of the anatomical tracts."
-	if [ ! -f $AnatomicalTractsFolder/DiffusionMeasurements.csv ]; then
+	echo "<wm_apply_ORG_atlas_to_subject> Report diffusion measurements of fiber clusters."
+	if [ ! -f $SeparatedClustersFolder/diffusion_measurements_commissural.csv ]; then
 		wm_diffusion_measurements.py \
-			$AnatomicalTractsFolder $AnatomicalTractsFolder $FiberTractMeasurementsCLI
+			$SeparatedClustersFolder/tracts_commissural $SeparatedClustersFolder/diffusion_measurements_commissural.csv $FiberTractMeasurementsCLI
+	else
+		echo " - diffusion measurements of commissural clusters has been done."
+	fi
+	if [ ! -f $SeparatedClustersFolder/diffusion_measurements_left_hemisphere.csv ]; then
+		wm_diffusion_measurements.py \
+			$SeparatedClustersFolder/tracts_left_hemisphere $SeparatedClustersFolder/diffusion_measurements_left_hemisphere.csv $FiberTractMeasurementsCLI
+	else
+		echo " - diffusion measurements of left hemisphere clusters has been done."
+	fi
+	if [ ! -f $SeparatedClustersFolder/diffusion_measurements_right_hemisphere.csv ]; then
+		wm_diffusion_measurements.py \
+			$SeparatedClustersFolder/tracts_right_hemisphere $SeparatedClustersFolder/diffusion_measurements_right_hemisphere.csv $FiberTractMeasurementsCLI
+	else
+		echo " - diffusion measurements of right hemisphere clusters has been done."
+	fi
+
+	if [ ! -f $SeparatedClustersFolder/diffusion_measurements_right_hemisphere.csv ]; then
+		echo ""
+		echo "ERROR: Reporting diffusion measurements of fiber clusters. failed. No diffusion measurement (.csv) files generated."
+		echo ""
+		exit
+	fi
+
+fi
+echo ""
+
+
+if [ $DiffMeasure == 1 ]; then
+
+	echo "<wm_apply_ORG_atlas_to_subject> Report diffusion measurements of the anatomical tracts."
+	if [ ! -f $AnatomicalTractsFolder/diffusion_measurements_anatomical_tracts.csv ]; then
+		wm_diffusion_measurements.py \
+			$AnatomicalTractsFolder $AnatomicalTractsFolder/diffusion_measurements_anatomical_tracts.csv $FiberTractMeasurementsCLI
 	else
 		echo " - diffusion measurements of anatomical tracts has been done."
 	fi
+
+	if [ ! -f $AnatomicalTractsFolder/diffusion_measurements_anatomical_tracts.csv ]; then
+		echo ""
+		echo "ERROR: Reporting diffusion measurements of fiber clusters. failed. No diffusion measurement (.csv) files generated."
+		echo ""
+		exit
+	fi
+
 fi
 echo ""
 
 if [ $CleanFiles == 1 ]; then
-	echo "<WMA_batch> Clean files using mininal removal."
+	echo "<wm_apply_ORG_atlas_to_subject> Clean files using mininal removal."
 	rm -rf $OutputCaseFolder/FiberClustering/InitialClusters
 	rm -rf $OutputCaseFolder/FiberClustering/TransformedClusters
 
 elif [ $CleanFiles == 2 ]; then
-	echo "<WMA_batch> Clean files using maximal removal."
+	echo "<wm_apply_ORG_atlas_to_subject> Clean files using maximal removal."
 	rm -rf $OutputCaseFolder/TractRegistration/*/output_tractography/*vtk
 	rm -rf $OutputCaseFolder/TractRegistration/*/iteration*
 	rm -rf $OutputCaseFolder/FiberClustering/InitialClusters/*
