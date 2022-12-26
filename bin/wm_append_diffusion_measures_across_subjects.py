9d6cbe46b9cf067f46149c3041fc19acc7e3ff8a
Fan Zhang <zhangfanmark@gmail.com>
2022-12-26 11:52:14 -0500

    Add scripts to combine measure files and compute the averaged measures

---
+++ bin/wm_append_diffusion_measures_across_subjects.py
@@ -0,0 +1,171 @@
+#!/usr/bin/env python
+#!/Library/Frameworks/EPD64.framework/Versions/Current/bin/ipython
+
+import numpy
+import argparse
+import pandas
+import os
+import glob
+
+#-----------------
+# Parse arguments
+#-----------------
+parser = argparse.ArgumentParser(
+    description="Append diffusion measure files from multiple subjects",
+    epilog="Written by Fan Zhang, fzhang@bwh.harvard.edu.")
+parser.add_argument(
+    'inputfolder',
+    help='A folder contains WMA output by subjects. Each subject should be one folder.')
+parser.add_argument(
+    'appenedmeasurefile',
+    help='Output csv file.')
+
+args = parser.parse_args()
+
+subject_folders = sorted(glob.glob(os.path.join(args.inputfolder, '*')))
+
+subject_folders = [d for d in subject_folders if os.path.isdir(d)]
+
+subject_IDs = [os.path.basename(folder) for folder in subject_folders]
+print()
+print('Subject IDs: (n=%d): ' % len(subject_IDs))
+print(subject_IDs)
+
+# anatomical tracts
+stats = pandas.read_table(os.path.join(subject_folders[0], 'AnatomicalTracts/diffusion_measurements_anatomical_tracts.csv'), delimiter=' , ')
+
+fields = [col.strip() for col in stats.columns]
+fields = fields[1:]
+print()
+print('Tract diffusion measures per subject: (n=%d): ' % len(fields))
+print(fields)
+
+tracts = stats.to_numpy()[:, 0]
+tracts = [os.path.basename(filepath).replace('.vtp', '').replace('.vtk', '').strip() for filepath in tracts]
+print()
+print('White matter tracts per subject: (n=%d): ' % len(tracts))
+print(tracts)
+
+appended_fields = ['subjectkey']
+for tract in tracts:
+    for field in fields:
+        appended_fields.append("%s.%s" % (tract, field))
+
+print()
+print('Appended tract diffusion measure fields per subject: (n=%d): ' % len(appended_fields))
+print(appended_fields[:10], ' ... ')
+
+data = []
+for s_idx, subject_folder in enumerate(subject_folders):
+    print(' * loading tract feature for subject #%04d (subject ID - %s):' %(s_idx, subject_IDs[s_idx]))
+    csv = os.path.join(subject_folder, 'AnatomicalTracts/diffusion_measurements_anatomical_tracts.csv')
+    stats = pandas.read_table(csv, delimiter=' , ')
+
+    stats_data = stats.to_numpy()[:, 1:]
+    stats_data_vec = stats_data.flatten()
+
+    if stats_data_vec.shape[0] != len(appended_fields) - 1:
+        print("Error: Check if the diffusion measure file has the same rows and columns with other subjects!")
+        exit()
+
+    data.append(stats_data_vec)
+
+data = numpy.array(data)
+data = numpy.concatenate([numpy.array(subject_IDs)[:, numpy.newaxis], data], axis = 1)
+
+df = pandas.DataFrame(data, columns=appended_fields)
+
+print()
+print('Appended tract diffusion measures:')
+print(df)
+
+df.to_csv(os.path.abspath(args.appenedmeasurefile).replace('.csv', '_anatomical_tracts.csv'), index=False)
+print()
+print('Output file at:', os.path.abspath(args.appenedmeasurefile).replace('.csv', '_anatomical_tracts.csv'))
+
+# Fiber clusters 
+stats = pandas.read_table(os.path.join(subject_folders[0], 'FiberClustering/SeparatedClusters/diffusion_measurements_commissural.csv'), delimiter=' , ')
+
+fields = [col.strip() for col in stats.columns]
+fields = fields[1:]
+print()
+print('Fiber cluster diffusion measures per subject: (n=%d): ' % len(fields))
+print(fields)
+
+clusters = stats.to_numpy()[:, 0]
+clusters = [os.path.basename(filepath).replace('.vtp', '').replace('.vtk', '').strip() for filepath in clusters]
+print()
+print('Fiber clusters per subject: (n=%d): ' % len(clusters))
+
+if len(clusters) == 800 and len(tracts) == 73:
+    comm_clusters = [3, 8, 33, 40, 46, 52, 56, 57, 62, 68, 69, 73, 86, 91, 109, 110, 114, 142, 144, 145, 146, 159, 163, 250, 251, 252, 257, 262, 263, 268, 271, 305, 311, 314, 322, 330, 334, 338, 342, 350, 363, 371, 375, 403, 410, 437, 440, 448, 456, 465, 468, 475, 484, 485, 488, 519, 522, 525, 543, 545, 549, 557, 576, 577, 582, 587, 591, 597, 601, 614, 620, 623, 627, 633, 645, 653, 658, 663, 670, 677, 683, 702, 770, 781]
+    comm_clusters = numpy.array(comm_clusters)
+    hemi_clusters = numpy.setdiff1d(numpy.arange(800), comm_clusters)
+elif len(clusters) == 800 and len(tracts) == 74: # CPC separated
+    comm_clusters = [3, 8, 33, 40, 46, 52, 56, 57, 62, 68, 69, 73, 86, 91, 109, 110, 114, 142, 144, 146, 163, 250, 251, 252, 257, 262, 263, 268, 271, 305, 311, 314, 322, 330, 334, 338, 342, 350, 363, 371, 375, 403, 410, 437, 440, 448, 456, 465, 468, 475, 484, 485, 488, 519, 522, 525, 543, 545, 549, 576, 577, 582, 587, 591, 597, 601, 614, 620, 623, 627, 633, 645, 653, 658, 663, 670, 683, 702, 781]
+    comm_clusters = numpy.array(comm_clusters)
+    hemi_clusters = numpy.setdiff1d(numpy.arange(800), comm_clusters)
+else:
+    comm_clusters = None
+    hemi_clusters = None
+
+locations = ['left_hemisphere', 'right_hemisphere', 'commissural']
+appended_fields = ['subjectkey']
+clusters = numpy.array(clusters)
+for loc in locations:
+    if loc == 'left_hemisphere' or loc == 'right_hemisphere':
+        clusters_loc = clusters[hemi_clusters]
+    else:
+        clusters_loc = clusters[comm_clusters]
+
+    for cluster in clusters_loc:
+        for field in fields:
+            appended_fields.append("%s.%s.%s" % (loc, cluster, field))
+
+print()
+print('Appended cluster diffusion measure fields per subject: (n=%d): ' % len(appended_fields))
+
+data = []
+for s_idx, subject_folder in enumerate(subject_folders):
+    print(' * loading cluster feature for subject #%04d (subject ID - %s):' %(s_idx, subject_IDs[s_idx]))
+    csv = os.path.join(subject_folder, 'FiberClustering/SeparatedClusters/diffusion_measurements_left_hemisphere.csv')
+    stats = pandas.read_table(csv, delimiter=' , ')
+
+    stats_data = stats.to_numpy()[:, 1:]
+    stats_data = stats_data[hemi_clusters, :]
+    l_stats_data_vec = stats_data.flatten()
+
+    csv = os.path.join(subject_folder, 'FiberClustering/SeparatedClusters/diffusion_measurements_right_hemisphere.csv')
+    stats = pandas.read_table(csv, delimiter=' , ')
+
+    stats_data = stats.to_numpy()[:, 1:]
+    stats_data = stats_data[hemi_clusters, :]
+    r_stats_data_vec = stats_data.flatten()
+
+    csv = os.path.join(subject_folder, 'FiberClustering/SeparatedClusters/diffusion_measurements_commissural.csv')
+    stats = pandas.read_table(csv, delimiter=' , ')
+
+    stats_data = stats.to_numpy()[:, 1:]
+    stats_data = stats_data[comm_clusters, :]
+    c_stats_data_vec = stats_data.flatten()
+
+    stats_data_vec = numpy.concatenate([l_stats_data_vec, r_stats_data_vec, c_stats_data_vec])
+
+    if stats_data_vec.shape[0] != len(appended_fields) - 1:
+        print("Error: Check if the diffusion measure file has the same rows and columns with other subjects!")
+        exit()
+
+    data.append(stats_data_vec)
+
+data = numpy.array(data)
+data = numpy.concatenate([numpy.array(subject_IDs)[:, numpy.newaxis], data], axis = 1)
+
+df = pandas.DataFrame(data, columns=appended_fields)
+
+print()
+print('Appended cluster diffusion measures:')
+print(df)
+
+df.to_csv(os.path.abspath(args.appenedmeasurefile).replace('.csv', '_fiber_clusters.csv'), index=False)
+print()
+print('Output file at:', os.path.abspath(args.appenedmeasurefile).replace('.csv', '_fiber_clusters.csv'))
---
+++ bin/wm_average_tract_measures.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+#!/Library/Frameworks/EPD64.framework/Versions/Current/bin/ipython
+
+import numpy
+import argparse
+import pandas
+import os
+import re
+
+#-----------------
+# Parse arguments
+#-----------------
+parser = argparse.ArgumentParser(
+    description="Compute averged statistics of the anatomical tracts. For diffusion measure with multiple statistics, only the Mean will be outputted.",
+    epilog="Written by Fan Zhang, fzhang@bwh.harvard.edu.")
+parser.add_argument(
+    'inputmeasure',
+    help='A .csv or .txt measurement files.')
+parser.add_argument(
+    'outoutmeasure',
+    help='Output csv file.')
+parser.add_argument(
+    '-appendedTractName', action="store", type=str,
+    help="Name of the appended tracts.")
+parser.add_argument(
+    '-tractList', action="store", type=str, nargs='+',
+    help='A list of tracts to be appended, e.g., AF_left AF_right.')
+
+args = parser.parse_args()
+
+stats = pandas.read_table(args.inputmeasure, delimiter=',')
+
+fields = []
+for col in stats.columns:
+    fields.append(col)
+fields = numpy.array(fields)
+
+print(fields)
+
+print('All avaiable tracts:')
+all_tracts = [f_name.replace('.Num_Points', '') for f_name in fields if f_name.endswith('.Num_Points')]
+print('N = ', str(len(all_tracts)), ':')
+print(all_tracts)
+
+append_list = []
+print('Tracts and related measures to be appended.')
+for t_idx, tract in enumerate(args.tractList):
+    print('*', t_idx, '-', tract)
+    indices = [index for index in range(len(fields)) if fields[index].startswith(tract+'.Num_') or re.search(tract+".*.Mean", fields[index])]
+    if len(indices) == 0:
+        print("Error: tract not founded in the input file.")
+        exit()
+    print(fields[indices])
+    append_list.append(indices)
+
+append_list = numpy.array(append_list)
+append_measures = [field.replace(args.tractList[-1], '') for field in fields[indices]]
+
+print("Output measures:", append_measures)
+
+avg_stats = [stats.to_numpy()[:, 0]]
+for m_idx, m_name in enumerate(append_measures):
+
+    if m_name == '.Num_Points':
+        
+        avg_stat = numpy.sum(stats.to_numpy()[:, append_list[:, m_idx]], axis=1)
+    
+    elif m_name == '.Num_Fibers':
+        
+        avg_stat = numpy.sum(stats.to_numpy()[:, append_list[:, m_idx]], axis=1)
+    
+    elif m_name == '.Mean_Length': # weighted by NoS
+        
+        weight = stats.to_numpy()[:, append_list[:, 1]]
+        val = stats.to_numpy()[:, append_list[:, m_idx]]
+
+        val_weighted_sum = numpy.sum(val * weight, axis=1)
+        weight_sum = numpy.sum(weight, axis=1)
+
+        empty_indices = numpy.where(weight_sum == 0)[0]
+        weight_sum[empty_indices] = 1
+        
+        avg_stat = val_weighted_sum / weight_sum
+        avg_stat[empty_indices] = 0
+        
+    else: # weighted by NoP
+
+        weight = stats.to_numpy()[:, append_list[:, 0]]
+        val = stats.to_numpy()[:, append_list[:, m_idx]]
+
+        val_weighted_sum = numpy.sum(val * weight, axis=1)
+        weight_sum = numpy.sum(weight, axis=1)
+
+        empty_indices = numpy.where(weight_sum == 0)[0]
+        weight_sum[empty_indices] = 1
+
+        avg_stat = val_weighted_sum / weight_sum
+        avg_stat[empty_indices] = -1
+
+    avg_stats.append(avg_stat)
+
+avg_stats = numpy.array(avg_stats)
+avg_stats = avg_stats.transpose()
+
+column_names = ['subjectkey']
+for m in append_measures:
+    column_names.append(args.appendedTractName+m)
+
+df = pandas.DataFrame(avg_stats, columns=column_names)
+
+print('Averaged tract measures:')
+print(df)
+
+df.to_csv(args.outoutmeasure, index=False)
+print()
+print('Output file at:', os.path.abspath(args.outoutmeasure))
+
+exit()
