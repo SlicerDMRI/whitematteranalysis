10e98e90380beb04757d9562120bf7285e2ec806
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-07-10 19:00:33 -0400

    STYLE: Fix typo in hemisphere location array name
    Fix typo in hemisphere location array name. Make the change be backwards
    compatible: allow reading tractograms that contain the misspelled
    property, but raise a warning.
    
    Add a script to fix the typo and re-write the tractograms that contain
    the misspelled property. Add the corresponding test.

---
+++ whitematteranalysis/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+hemisphere_loc_name_typo_warn_msg = (
+    'Hemisphere location scalar is described by the HemisphereLocataion name, '
+    'which contains a typo. Reading files that contain the typo will not be '
+    'supported in future releases. Use whitematteranalysis to re-write such '
+    'files.')
