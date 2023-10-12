961ecf64cd6fd4bed88d8bbf35a6d27ee75fcd41
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2023-10-12 15:09:46 -0400

    ENH: Relocate contents in `test_run.py` to tests
    Relocate contents in `test_run.py` to the appropriate tests:
    - The file is removed, and thus removed from the scripts list in
      `setup.py`.
    - The relocated contents are commented out as no testing data exists
      yet.
    
    Allows tests to pass. Fixes:
    ```
    ==================================== ERRORS ====================================
    _____________________ ERROR collecting testing/test_run.py _____________________
    (...)
    testing/test_run.py:17: in <module>
        wm_quality_control_tractography.main()
    bin/wm_quality_control_tractography.py:56: in main
        exit()
    /opt/hostedtoolcache/Python/3.8.18/x64/lib/python3.8/_sitebuiltins.py:26: in __call__
        raise SystemExit(code)
    E   SystemExit: None
    ------------------------------- Captured stdout --------------------------------
    Importing whitematteranalysis package.
    <wm_quality_control_tractography.py> Starting...
    <wm_quality_control_tractography.py> Error: Input directory input_tractography/ does not exist.
    ```
    
    raised for example in:
    https://github.com/SlicerDMRI/whitematteranalysis/actions/runs/6499690724/job/17653542925?pr=157#step:6:849

--- bin/tests/test_wm_cluster_remove_outliers.py
+++ bin/tests/test_wm_cluster_remove_outliers.py
@@ -4,3 +4,16 @@
 def test_help_option(script_runner):
     ret = script_runner.run(["wm_cluster_remove_outliers.py", "--help"])
     assert ret.success
+
+
+# def test_execution(script_runner):
+    # in_tractography_fname = "subject_cluster_output/bundle_reg"
+    # in_atlas_fname = "ORG-800FC-100HCP"
+    # out_dirname = "ORG-800FC-100HCP"
+    # ret = script_runner.run(
+    #    "wm_cluster_remove_outliers.py",
+    #    in_tractography_fname,
+    #    in_atlas_fname,
+    #    out_dirname,
+    # )
+    # assert ret.success
--- bin/tests/test_wm_quality_control_tractography.py
+++ bin/tests/test_wm_quality_control_tractography.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 def test_help_option(script_runner):
     ret = script_runner.run(["wm_quality_control_tractography.py", "--help"])
     assert ret.success
+
+
+# def test_execution(script_runner):
+    # in_dirname = "input_tractography"
+    # out_dirname = "qc_output"
+    # ret = script_runner.run(
+    #    "wm_quality_control_tractography.py", in_dirname, out_dirname,
+    # )
+    # assert ret.success
